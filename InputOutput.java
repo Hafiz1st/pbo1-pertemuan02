6d292d8bf63ac53ee8e2f803834b8b9cac4ee728
Hafiz1st <hafiz100st@gmail.com>
2020-03-25 02:20:40 -0700

    Revert "Buat input String kedalam Variable"
    This reverts commit c5308a02fcdafb059bdc6037cef6527fc3e27099.

--- InputOutput.java
+++ InputOutput.java
@@ -3,9 +3,5 @@ import java.util.Scanner;
 public class inputOutput{
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
-        String nama;
-        System.out.println("Masukkan nama anda: ");
-        nama = scanner.nextLine();
-        
     }
 }
