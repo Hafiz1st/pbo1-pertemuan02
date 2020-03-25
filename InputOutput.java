df2dda8d3f207f8a4fb9510ea6f1d44bf676d357
Hafiz1st <hafiz100st@gmail.com>
2020-03-25 02:54:04 -0700

    Penambahan Fungsi Mengetahui Umur di InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 
-public class inputOutput{
+public class InputOutput{
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         String nama;
-        System.out.println("Masukkan nama anda: ");
+        int tahunLahir;
+        int umur;
+        int tahun;
+        tahun = 2020;
+
+        System.out.print("Masukkan nama anda: ");
         nama = scanner.nextLine();
-        System.out.println("Hai, "+nama)
+        System.out.println("Hai, " +nama);
+        
+        System.out.print("Masukkan Tahun Anda Lahir: ");
+        tahunLahir = scanner.nextInt();
+        
+        umur = tahun - tahunLahir;
+        System.out.print("Umur Anda Adalah: " +umur);
     }
 }
